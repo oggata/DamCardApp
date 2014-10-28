95836d31abd2c2bee5e71d8834a44700bc1e39a2
ogata.fumitoshi <oggata@gmail.com>
2014-10-28 17:12:04 +0900

    small refactoring

--- DamCardApp/DamsListViewController.swift
+++ DamCardApp/DamsListViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class DamsListViewController: UIViewController {
     
+    @IBOutlet var damsList: UITableView!
+    var damsData:NSArray = NSArray()
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadData()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -20,5 +23,38 @@ class DamsListViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // Parseからデータの取得
+    func loadData(){
+        Parse.setApplicationId("dBzkl9gkGPsQoyRHq5WOv9wzbUmK9QEhJXBpO6mf",clientKey: "HtkhZciPZ3p5M8elvwJBrI1ORvhBgU95bOSjCRJ2")
+        
+        var query:PFQuery = PFQuery(className: "Dams")
+        //query.whereKey("user", equalTo: PFUser.currentUser())
+        query.findObjectsInBackgroundWithBlock{
+            (objects:[AnyObject]!, error:NSError!)->Void in
+            if error != nil{
+                //println("error occured")
+                //println(error)
+            }else{
+                //println("got data")
+                let array:NSArray = objects
+                self.damsData = objects
+                self.damsList.reloadData()
+            }
+        }
+    }
+
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int  {
+        return damsData.count as Int
+    }
+    
+    func tableView(tableView: UITableView?, cellForRowAtIndexPath indexPath:NSIndexPath!) -> UITableViewCell! {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "xxxx"
+        cell.textLabel?.text = self.damsData[indexPath.row]["name"] as String
+        return cell
+    }
+    
+    func tableView(tableView: UITableView?, didSelectRowAtIndexPath indexPath:NSIndexPath!) {
+    }
     
 }
--- DamCardApp/PrefectureListViewController.swift
+++ DamCardApp/PrefectureListViewController.swift
@@ -8,15 +8,40 @@
 
 import UIKit
 
+
 class PrefectureListViewController: UIViewController {
     
     @IBOutlet var prefectureList: UITableView!
-
+    typealias Prifecture = Dictionary<String, String>
+    var PrifecturesArray: [Prifecture] = [
+        ["name": "default", "en": "default"],
+    ]
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadJSONFile()
     }
-    
+        
+    func loadJSONFile() -> NSArray {
+        var filePath:String? = NSBundle.mainBundle().pathForResource("prefecture",ofType:"json") as String?
+        var err: NSError
+        
+        var fileHandle : NSFileHandle = NSFileHandle(forReadingAtPath: filePath!)
+        var acceptData : NSData = fileHandle.readDataToEndOfFile()
+        let str = NSString(data:acceptData, encoding:NSUTF8StringEncoding)
+        var hogeJson = JSON.parse(str)
+        for (i, v) in hogeJson {
+            //println(v["name"].asString!)
+            //println(v["en"].asString!)
+            var _data = [
+                "name":v["name"].asString!,
+                "en":v["name"].asString!
+            ]
+            PrifecturesArray.append(_data)
+        }
+        return PrifecturesArray
+    }
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -24,13 +49,12 @@ class PrefectureListViewController: UIViewController {
     
  
     func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int  {
-        return 10
-        //return self.timelineData2.count as Int
+        return PrifecturesArray.count as Int
     }
     
     func tableView(tableView: UITableView?, cellForRowAtIndexPath indexPath:NSIndexPath!) -> UITableViewCell! {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
-        //cell.textLabel?.text = self.timelineData2[indexPath.row]["name"] as String
+        cell.textLabel?.text = PrifecturesArray[indexPath.row]["name"] as String!
         //cell.detailTextLabel?.text = self.timelineData[indexPath.row][0]["comment"];
         return cell
     }
@@ -39,7 +63,7 @@ class PrefectureListViewController: UIViewController {
     func tableView(tableView: UITableView?, didSelectRowAtIndexPath indexPath:NSIndexPath!) {
         //requestURL = "http://qiita.com/"
         //requestURL = ArticleArray[indexPath.row][0]["url"]
-        //performSegueWithIdentifier("TRADITION_ArticleDetailVIew",sender: nil)
+        performSegueWithIdentifier("traditionToDamsListView",sender: nil)
     }
 }
 
