23f31801828d37bb173aad7bac3e1f1d49c4357c
ogata.fumitoshi <oggata@gmail.com>
2014-10-30 15:39:12 +0900

    add comment view

---
+++ DamCardApp/CustomPresentationController.swift
@@ -0,0 +1,9 @@
+//
+//  CustomPresentationController.swift
+//  DamCardApp
+//
+//  Created by Fumitoshi Ogata on 2014/10/30.
+//  Copyright (c) 2014年 Fumitoshi Ogata. All rights reserved.
+//
+
+import Foundation
---
+++ DamCardApp/DamCommentViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DamCommentViewController.swift
+//  DamCardApp
+//
+//  Created by Fumitoshi Ogata on 2014/10/30.
+//  Copyright (c) 2014年 Fumitoshi Ogata. All rights reserved.
+//
+
+
+import UIKit
+
+class DamCommentViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+
