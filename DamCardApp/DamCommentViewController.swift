f1964943b13f599d0cf0935468083c35ca810cef
ogata.fumitoshi <oggata@gmail.com>
2014-10-30 16:39:13 +0900

    add write comment view

--- DamCardApp/DamCommentViewController.swift
+++ DamCardApp/DamCommentViewController.swift
@@ -9,7 +9,21 @@
 
 import UIKit
 
-class DamCommentViewController: UIViewController {
+class DamCommentViewController: UIViewController, UIViewControllerTransitioningDelegate {
+    
+    @IBOutlet var writeButton: UIButton!
+
+    @IBAction func writeButtonDidTouch(sender: AnyObject) {
+println("did touch!!!!")
+        // 新しい View Controller をモーダル表示する
+        
+        let controller: UINavigationController! = self.storyboard?.instantiateViewControllerWithIdentifier("NavigationController") as? UINavigationController
+        controller.modalPresentationStyle = .Custom
+        controller.transitioningDelegate = self
+        self.presentViewController(controller, animated: true, completion: {
+        })
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +34,86 @@ class DamCommentViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController!, sourceViewController source: UIViewController) -> UIPresentationController? {
+        return CustomPresentationController(presentedViewController: presented, presentingViewController: presenting)
+    }
 
+}
+
+
+
+
+class CustomPresentationController: UIPresentationController {
+    
+    // 呼び出し元の View Controller の上に重ねるオーバーレイ View
+    var overlay: UIView!
+    
+    // 表示トランジション開始前に呼ばれる
+    override func presentationTransitionWillBegin() {
+        let containerView = self.containerView
+        let presented = self.presentedViewController
+        
+        self.overlay = UIView(frame: containerView.bounds)
+        self.overlay.gestureRecognizers = [UITapGestureRecognizer(target: self, action: "overlayDidTouch:")]
+        self.overlay.backgroundColor = UIColor.blackColor()
+        self.overlay.alpha = 0.0
+        containerView.insertSubview(self.overlay, atIndex: 0)
+        
+        // トランジションを実行
+        presentedViewController.transitionCoordinator()?.animateAlongsideTransition({
+            [unowned self] context in
+            self.overlay.alpha = 0.5
+            }, completion: nil)
+    }
+    
+    // 非表示トランジション開始前に呼ばれる
+    override func dismissalTransitionWillBegin() {
+        self.presentedViewController.transitionCoordinator()?.animateAlongsideTransition({
+            [unowned self] context in
+            self.overlay.alpha = 0.0
+            }, completion: nil)
+    }
+    
+    // 非表示トランジション開始後に呼ばれる
+    override func dismissalTransitionDidEnd(completed: Bool) {
+        if completed {
+            self.overlay.removeFromSuperview()
+        }
+    }
+    
+    // 子のコンテナのサイズを返す
+    override func sizeForChildContentContainer(container: UIContentContainer, withParentContainerSize parentSize: CGSize) -> CGSize {
+        return CGSize(width: parentSize.width * 4/5, height: parentSize.height)
+    }
+    
+    // 呼び出し先の View Controller の Frame を返す
+    override func frameOfPresentedViewInContainerView() -> CGRect {
+        var presentedViewFrame = CGRectZero
+        let containerBounds = self.containerView.bounds
+        presentedViewFrame.size = self.sizeForChildContentContainer(self.presentedViewController, withParentContainerSize: containerBounds.size)
+        //presentedViewFrame.origin.x = containerBounds.size.width - presentedViewFrame.size.width
+        presentedViewFrame.origin.x = 30
+        //presentedViewFrame.origin.y = containerBounds.size.height - presentedViewFrame.size.height
+        presentedViewFrame.origin.y = 200
+        return presentedViewFrame
+    }
+    
+    // レイアウト開始前に呼ばれる
+    override func containerViewWillLayoutSubviews() {
+        overlay.frame = self.containerView.bounds
+        self.presentedView().frame = self.frameOfPresentedViewInContainerView()
+    }
+    
+    // レイアウト開始後に呼ばれる
+    override func containerViewDidLayoutSubviews() {
+    }
+    
+    // オーバーレイの View をタッチしたときに呼ばれる
+    func overlayDidTouch(sender: AnyObject) {
+        self.presentedViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 }
 
 
---
+++ DamCardApp/WriteCommentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WriteCommentViewController.swift
+//  DamCardApp
+//
+//  Created by Fumitoshi Ogata on 2014/10/30.
+//  Copyright (c) 2014年 Fumitoshi Ogata. All rights reserved.
+//
+
+import UIKit
+
+class WriteCommentViewController: UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
+
+
+
+
+
