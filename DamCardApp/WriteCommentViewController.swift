6c1bbf27a317757ed3592a9cc0d7e3134ffba9f2
ogata.fumitoshi <oggata@gmail.com>
2014-10-30 17:03:37 +0900

    add text input for comment

--- DamCardApp/WriteCommentViewController.swift
+++ DamCardApp/WriteCommentViewController.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class WriteCommentViewController: UIViewController {
         
+    @IBOutlet var cancelButton: UIButton!
+    @IBAction func cancelButtonDidTouch(sender: AnyObject) {
+         self.navigationController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBOutlet var sendButton: UIButton!
+    @IBAction func sendButtonDidTouch(sender: AnyObject) {
+        self.navigationController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBOutlet var inputComment: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        inputComment.frame = CGRectMake(0, 0, 300, 300)
+        inputComment.allowsEditingTextAttributes = true
+        inputComment.layer.borderWidth = 1
+        inputComment.layer.borderColor = UIColor.blackColor().CGColor
+        inputComment.layer.cornerRadius = 8
         // Do any additional setup after loading the view, typically from a nib.
     }
     
