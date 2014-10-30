b12cdfb3852c7a69ff1e819b5d4cb35672ba0723
ogata.fumitoshi <oggata@gmail.com>
2014-10-30 15:10:32 +0900

    add columns to tsv data

--- create_data.rb
+++ create_data.rb
@@ -5,14 +5,22 @@ Parse.init :application_id => "dBzkl9gkGPsQoyRHq5WOv9wzbUmK9QEhJXBpO6mf",
            :api_key        => "oP67U8lHvKap1wKKXNajE4T2vPENtWQ7klx43laO"
 
 Encoding.default_external = 'UTF-8'
+
+#Delete ALL Rows
+dams_all_cnt = Parse::Query.new("Dams").get.length
+for num in 1..dams_all_cnt do
+	del_target = Parse::Query.new("Dams").get.first
+	del_target.parse_delete
+end
+
+#Create Row From TSV File
 File.open("dams.tsv","r") do |file|
-  file.each do |row|
-    #print "\n"
+  file.each_line do |row|
   	columns = row.split("\t")
 	dams = Parse::Object.new('Dams')
-	#xlsManageId = data[0]
-	#print xlsManageId
-	dams[:xlsManageId] = 1
+	#print columns[1]
+	#print "\n"
+	dams[:xlsManageId] = columns[0].to_i
 	dams[:name] = columns[1]
 	dams[:versionNumber] = 1
 	dams[:distributionPlaceName] = columns[3]
@@ -20,8 +28,8 @@ File.open("dams.tsv","r") do |file|
 	dams[:prefectureName] = columns[5]
 	dams[:address] = columns[6]
 	dams[:url] = columns[7]
+	dams[:isOpenWeekEnd] = columns[8].to_i
 	dams.save
   end
 end
-
 
