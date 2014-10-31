fb4c2a89fdab1fcc3d84f4bfc6d419b65079e115
ogata.fumitoshi <oggata@gmail.com>
2014-10-31 16:27:30 +0900

    remove files

--- DamCardApp/CustomPresentationController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CustomPresentationController.swift
-//  DamCardApp
-//
-//  Created by Fumitoshi Ogata on 2014/10/30.
-//  Copyright (c) 2014年 Fumitoshi Ogata. All rights reserved.
-//
-
-import Foundation
